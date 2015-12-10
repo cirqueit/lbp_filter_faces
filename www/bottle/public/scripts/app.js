4e46a56bc678385c06308e2c440b54bbc4a62258
cirqueit <handbalancing@gmail.com>
2015-12-10 17:14:33 -0500

    removed public

--- www/bottle/public/scripts/app.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  angular.module('flatuiApp', ['flatuiApp.controllers']);
-  return angular.module('flatuiApp.controllers', []);
-})();
-
-//# sourceMappingURL=../maps/app.js.map
--- www/bottle/public/scripts/main.js
+++
@@ -1,3 +0,0 @@
-console.log('hello world');
-
-//# sourceMappingURL=../maps/main.js.map
